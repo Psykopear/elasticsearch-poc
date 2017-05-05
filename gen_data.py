06ac5b225f11541ed5f505e5a90cf17ad4eec9d8
Federico Dolce <psykopear@gmail.com>
2017-05-05 16:48:39 +0200

    Added jump length and jump height metrics

--- gen_data.py
+++ gen_data.py
@@ -12,22 +12,6 @@ def generate_uuid_list(num=10):
 # Generate users
 USER_IDS = generate_uuid_list(50)
 
-# Possible metric types
-METRIC_TYPES = [
-    'time',
-    # 'jump_height',
-    # 'jump_length',
-]
-
-VALUES = {
-    # Let's assume time in seconds
-    'time': [random.randint(1000, 9999999) for i in range(1000)],
-    # Jump height in cm
-    # 'jump_height': [random.randint(50, 500) for i in range(1000)],
-    # jump length in cm
-    # 'jump_length': [random.randint(50, 1000) for i in range(1000)]
-}
-
 RESOURCE_TYPES = [
     'segment',
     'recording',
@@ -41,20 +25,38 @@ RESOURCE_IDS = {
     'activity': generate_uuid_list(100),
 }
 
+# Possible metric types
+METRIC_TYPES = {
+    'segment': ['time', 'jump_height', 'jump_length'],
+    'recording': ['time', 'jump_height', 'jump_length'],
+    'activity': ['time'],
+}
+
+VALUES = {
+    # Let's assume time in seconds
+    'time': [random.randint(1000, 9999999) for i in range(1000)],
+    # Jump height in cm
+    'jump_height': [random.randint(50, 500) for i in range(1000)],
+    # jump length in cm
+    'jump_length': [random.randint(50, 1000) for i in range(1000)]
+}
+
+
 
 def gen_row():
     """
     Generate a meaningfull dict of values representing a record
     """
     user_id = random.choice(USER_IDS)
-    timestamp = datetime.now() - timedelta(days=random.randint(0, 365), 
+    timestamp = datetime.now() - timedelta(days=random.randint(0, 365),
                                            milliseconds=random.randint(0, 100000000))
-
-    metric_type = random.choice(METRIC_TYPES)
-    value = random.choice(VALUES[metric_type])
 
     resource_type = random.choice(RESOURCE_TYPES)
     resource_id = random.choice(RESOURCE_IDS[resource_type])
+
+    metric_type = random.choice(METRIC_TYPES[resource_type])
+    value = random.choice(VALUES[metric_type])
+
     data = {
         "metric_type": metric_type,
         "user_id": user_id,
